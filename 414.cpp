57f8c7f84502f8fdae95146d07cee2e789721086
Your Name <you@example.com>
2020-10-20 19:49:36 +0530

    imthebest

--- 414.cpp
+++ 414.cpp
@@ -1,18 +1,54 @@
 #include <vector>
-#include <algorithm>
+#include <limits.h>
 #include <iostream>
 using namespace std;
 
 int thirdMax(vector<int> &nums)
 {
 
-    sort(nums.begin(), nums.end());
-int count = 0;
+    int first = LONG_MIN;
+    int second = LONG_MIN;
+    int third = LONG_MIN;
 
-    for(int i = nums.begin(); i! = nums.end(); i++)
+    for (int i = 0; i < nums.size(); i++)
     {
-        int ans = nums[i];
-        
+        if (nums[i] == first || nums[i] == second || nums[i] == third)
+            continue;
+        cout << "nums[i]: " << nums[i] << endl;
+        if (nums[i] > first)
+        {
+            third = second;
+            second = first;
+            first = nums[i];
+            cout << "first: " << first << " second: " << second << " third " << third << endl;
+            continue;
+        }
+        if (nums[i] > second)
+        {
+            third = second;
+            second = nums[i];
+            cout << "first: " << first << " second: " << second << " third " << third << endl;
+            continue;
+        }
+
+        if (nums[i] > third)
+        {
+            third = nums[i];
+            cout << "first: " << first << " second: " << second << " third " << third << endl;
+            continue;
+        }
     }
 
+    if (third == LONG_MIN)
+        return first;
+
+    return third;
+}
+
+int main()
+{
+
+    vector<int> nums{3, 1};
+    cout << thirdMax(nums);
+    return 0;
 }
